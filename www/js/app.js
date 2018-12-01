1ea4e9aeb8bbad819ac88247993b83a8c637b46d
Matheus Gois <matheusgoislimasilva@gmail.com>
2018-12-01 02:01:45 -0200

    Add files via upload

--- www/js/app.js
+++ www/js/app.js
@@ -1,26 +1,29 @@
 var $ = Dom7;
 
+var DB = new dataBank();
 
 // Init App
 var app = new Framework7({
   name: 'App',
   id: 'br.mackenzie.lfs.App',
   root: '#app',
-  theme: 'md',
-
-
-  routes: [
-    {
-      path: '/about/',
-      url: './pages/about.html',
-    }, {
-      path: '/menu/',
-      componentUrl: './pages/menu.html'
-    }, {
-      path: '/grupo/',
-      componentUrl: './pages/grupo.html'
-    }
-  ]
+  theme: 'auto',
+  routes: [{
+    path: '/about/',
+    url: './pages/about.html',
+  }, {
+    path: '/login/',
+    componentUrl: './pages/login.html',
+  }, {
+    path: '/menu/',
+    componentUrl: './pages/menu.html'
+  }, {
+    path: '/index/',
+    url: './index.html',
+  }, {
+    path: '/profile/',
+    componentUrl: './pages/profile.html'
+  }]
 });
 
 var mainView = app.views.create('.view-main');
